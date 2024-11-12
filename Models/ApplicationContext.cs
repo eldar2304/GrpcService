f757bf8c15f8ded43ba22045f03b4f20bb492463
eldar2304 <safarovef@gmail.com>
2024-11-12 15:37:17 +0300

    Add project files.

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GrpcService.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public ApplicationContext()
+        {
+         
+            Database.EnsureCreated();
+
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=mydb; UserName=postgres;Password=postgres");
+        }
+
+    }
+}
