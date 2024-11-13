0286d7819734b320cebced2da320e7cdc03e895f
eldar2304 <safarovef@gmail.com>
2024-11-13 14:13:40 +0300

    убрал стандартные файлы, которые шли с шаблоном

---
+++ Services/UserAccountService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using GrpcService.Models;
+
+namespace GrpcService.Services
+{
+
+    public class UserAccountService : UserAccount.UserAccountBase
+    {
+        private readonly ILogger<UserAccountService> _logger;
+        public UserAccountService(ILogger<UserAccountService> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public override Task<UserReply> GetUserById(UserRequest request, ServerCallContext context)
+        {
+            ApplicationContext contextDb = new ApplicationContext();
+            Client res = contextDb.Clients.Where(x => x.Id == request.Id).ToList().FirstOrDefault(new Client());
+
+            return Task.FromResult(new UserReply
+            {
+                Name = res.Name,
+                Phone = res.Phone
+            });
+            
+        }
+    }
+}
